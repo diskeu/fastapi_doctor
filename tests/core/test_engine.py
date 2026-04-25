a6834cb942ff864596fc52425289401a7a399687
Tim Jelenz <jelenzt@gmail.com>
2026-04-25 12:21:02 +0200

    test: add `test_extract_routes`
    - test will check if the `extract_routes` function works correctly

---
+++ tests/core/test_engine.py
@@ -0,0 +1,30 @@
+from typing import Callable
+from fastapi.applications import FastAPI
+from fastapi.testclient import TestClient
+from fastapi import APIRouter
+from fastapi_doctor.core.engine import (
+    extract_routes,
+    RouteInfo
+)
+
+
+def test_extract_routes() -> None:
+    router = APIRouter()
+
+    @router.get("/")
+    def foo():
+        return {"123": "abc"}
+    @router.get("/fizz")
+    def fizz():
+        return {"123": "abc"}
+
+    app = FastAPI()
+    app.include_router(router)
+    routes = extract_routes(app)
+
+    assert routes
+    for route in routes:
+        assert isinstance(route, RouteInfo)
+        assert isinstance(route.path, str)
+        assert callable(route.endpoint)
+        assert isinstance(route.name, str)
