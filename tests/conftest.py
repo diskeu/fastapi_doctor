b69854eba8b17546058b09e7b9bbc05bc4f947d2
Tim Jelenz <jelenzt@gmail.com>
2026-04-30 22:02:43 +0200

    fix: move unreachable fixtures into `confest.py`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,20 @@
 from fastapi_doctor.core.protocol import RouteInfo, Issue
+from functools import partial
+from typing import Literal, Any
+from fastapi.testclient import TestClient
 import pytest
+
+
+@pytest.fixture
+def test_client_factory(
+    anyio_backend_name: Literal["asyncio", "Trio"],
+    anyio_backend_options: dict[str, Any]
+):
+    return partial(
+        TestClient,
+        backend=anyio_backend_name,
+        backend_options=anyio_backend_options 
+    )
 
 
 @pytest.fixture
