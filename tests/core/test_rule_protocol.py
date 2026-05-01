470274e9d91bccc941d6068c53af78422693b2fb
Tim Jelenz <jelenzt@gmail.com>
2026-05-01 17:35:09 +0200

    add: test case for testing `Rule.iter_registry_sorted()`
    - creates 2 subclasses with different prioritie and evaluates if
      `Rule.iter_registry_sorted()` yields the subclasses in the correct order
      after their priority

--- tests/core/test_rule_protocol.py
+++ tests/core/test_rule_protocol.py
@@ -11,6 +11,17 @@ def test_rule_iter_registry(rule_subclass_factory):
     assert next(rules_generator) is test_rule2
 
 
+def test_rule_iter_registry_sorted(rule_subclass_factory):
+    test_rule1 = rule_subclass_factory()  
+    test_rule2 = rule_subclass_factory()
+    test_rule1.priority, test_rule2.priority = 8, 4
+
+    rules_generator = Rule.iter_registry_sorted()
+
+    assert next(rules_generator) is test_rule2
+    assert next(rules_generator) is test_rule1
+
+
 def test_add_invalid_rule(
     rule_call_method,
     rule_description_property,
