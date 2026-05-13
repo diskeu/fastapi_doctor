174462811516f73eff337d046cced1363dc6e809
Tim Jelenz <jelenzt@gmail.com>
2026-05-13 16:59:20 +0200

    refactor: remove attribute checking with `rule` decorator
    - Creating an instance of `Rule` via the `rule` decorator, should be exactly
      the same as inheriting from `Rule`.
    - Having early attribute checking on the methods from the class, which the
    decorator recieved, wouldn't fulfill this consistency.

--- fastapi_doctor/core/auto_registration.py
+++ fastapi_doctor/core/auto_registration.py
@@ -1,23 +1,28 @@
 from fastapi_doctor.core.protocol import Rule
 from typing import Any, Callable
 
+
 def rule(*, config: dict[str, Any] | None = None, priority: int = 0) -> Callable[[type[Any]], type[Rule]]:
     if not config: config = {}
 
     def inner(cls) -> type[Rule]:
-        # Due classes also don't get checked on the correct implementation
-        # of abstract methods, it'd be better to not check them directly
-        # when creating them with the `type` function.
         name: str = cls.__name__
-        bases: tuple[type[Any], ...] = (Rule, )
-        methods: dict[str, Any] = {
-            "__call__": cls.call,
-            "description": cls.description,
-            "supports": cls.supports
-        }
-        rule: type[Rule] = type(name, bases, methods)
 
+        bases: tuple[type[Any], ...] = (Rule, )
+
+        methods: dict[str, Any] = {
+            method_name: getattr(cls, method_name)
+            for method_name in [
+                "__call__",
+                "description",
+                "supports"
+            ]
+            if hasattr(cls, method_name)
+        }
+
+        rule: type[Rule] = type(name, bases, methods)
         rule.config = cls.config | config
         rule.priority = priority
+
         return rule
     return inner
