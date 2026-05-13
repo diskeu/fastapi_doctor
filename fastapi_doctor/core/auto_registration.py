9b51e9754902e232e7976f01201caada5a0b65d7
Tim Jelenz <jelenzt@gmail.com>
2026-05-13 16:46:49 +0200

    fix: `rule` decorator type annotations, false return value and parameter
    - fix return value to return new class instead of given class
    - fix modifying methods of given class instead of created one
    - fix type annotations of `[name, bases and methods]`
    - makes `rule` `config` parameter optional

--- fastapi_doctor/core/auto_registration.py
+++ fastapi_doctor/core/auto_registration.py
@@ -1,25 +1,23 @@
 from fastapi_doctor.core.protocol import Rule
-from typing import Any
+from typing import Any, Callable
 
-def rule(*, config: dict[str, Any], priority: int = 0):
-    def inner(cls):
+def rule(*, config: dict[str, Any] | None = None, priority: int = 0) -> Callable[[type[Any]], type[Rule]]:
+    if not config: config = {}
+
+    def inner(cls) -> type[Rule]:
         # Due classes also don't get checked on the correct implementation
         # of abstract methods, it'd be better to not check them directly
         # when creating them with the `type` function.
-        # for method in ("__check__", "description", "supports"):
-        #     if not hasattr(cls, method):
-        #         raise TypeError(f"{cls} does not support `{method}`.")
-        rule = type(
-            name="rule",
-            bases=(Rule,),
-            dict={
-                "__check__": cls.check,
-                "description": cls.description,
-                "supports": cls.supports
-            }
-        )
-        cls.config = cls.config | config
-        cls.priority = priority
-        Rule._registry.append(cls)
-        return cls
+        name: str = cls.__name__
+        bases: tuple[type[Any], ...] = (Rule, )
+        methods: dict[str, Any] = {
+            "__call__": cls.call,
+            "description": cls.description,
+            "supports": cls.supports
+        }
+        rule: type[Rule] = type(name, bases, methods)
+
+        rule.config = cls.config | config
+        rule.priority = priority
+        return rule
     return inner
