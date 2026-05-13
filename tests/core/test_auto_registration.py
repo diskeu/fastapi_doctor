0fdadcc78dfcb66905fb0ff62ac389a4dbccaefc
Tim Jelenz <jelenzt@gmail.com>
2026-05-13 18:31:39 +0200

    add: test case for `test_valid_class_auto_registration`

--- tests/core/test_auto_registration.py
+++ tests/core/test_auto_registration.py
@@ -1,9 +1,25 @@
 from fastapi_doctor.core.auto_registration import rule
-from fastapi_doctor.core.protocol import Rule
+from fastapi_doctor.core.protocol import Rule, RouteInfo
 
 
-def test_valid_class_auto_registration_():
-    ...
+def test_valid_class_auto_registration():
+    @rule()
+    class ValidRule():
+        def __call__(self, route: RouteInfo):
+            ...
+
+        @property
+        def description(self):
+            ...
+
+        def supports(self, route: RouteInfo):
+            ...
+    valid_rule = ValidRule()
+    assert ValidRule in Rule._registry
+
+    valid_rule(None)
+    valid_rule.description
+    valid_rule.supports(None)
 
 
 def test_invalid_class_auto_registration_():
