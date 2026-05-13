f9877ec696085f6b0262f01d7ebae8f5205dac62
Tim Jelenz <jelenzt@gmail.com>
2026-05-13 17:26:34 +0200

    add: dummy test functions
    Placeholders for:
    - `test_valid_class_auto_registration`
    - `test_invalid_class_auto_registration`
    - `test_rule_function_parameters`
    - `test_class_declaration_values_overwrite`

---
+++ tests/core/test_auto_registration.py
@@ -0,0 +1,23 @@
+from fastapi_doctor.core.auto_registration import rule
+from fastapi_doctor.core.protocol import Rule
+
+
+def test_valid_class_auto_registration_():
+    ...
+
+
+def test_invalid_class_auto_registration_():
+    ...
+
+
+def test_rule_function_parameters():
+    ...
+
+
+def test_class_declaration_values_overwrite():
+    """
+    Test declaring `config and priority` in class and overwritting
+    them with the `rule` decorator function parameters.
+    """
+    ...
+
