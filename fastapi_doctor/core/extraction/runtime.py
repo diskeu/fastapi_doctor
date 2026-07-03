4a62fd484457402b0461743aabfe2474dfac1528
Tim Jelenz <jelenzt@gmail.com>
2026-07-03 14:11:24 +0200

    fix: add support for fastapi versions older than 0.137.0
    - fastapi versions where `app.routes` is a flat list or versions where
      `iter_route_context` is already introduced are now supported

--- fastapi_doctor/core/extraction/runtime.py
+++ fastapi_doctor/core/extraction/runtime.py
@@ -1,32 +1,53 @@
+from typing import Iterator
 from fastapi.applications import FastAPI
-# iter_route_contexts is only supported in fastapi >= 0.137.2
-from fastapi.routing import APIRoute, iter_route_contexts
+from fastapi.routing import APIRoute
 from fastapi_doctor.core.protocol import RouteInfo
-from starlette.routing import BaseRoute
+
+# iter_route_contexts is only supported in fastapi >= 0.137.2
+# since `app.routes` got refactored in 0.137.0, version 0.137.0
+# and 0.137.1 are not supported
+try:
+    from fastapi.routing import iter_route_contexts
+    from starlette.routing import BaseRoute
+
+    def _iter_api_routes(app: FastAPI) -> Iterator[APIRoute]:
+        for ctx in iter_route_contexts(app.routes):
+            route: BaseRoute = ctx.original_route
+            if isinstance(route, APIRoute):
+                yield route
+
+except ImportError:
+
+    def _iter_api_routes(app: FastAPI) -> Iterator[APIRoute]:
+        for route in app.routes:
+            print("ROUTE ", route)
+            if isinstance(route, APIRoute):
+                yield route
+        # return (
+        #     route for route in app.routes
+        #     if isinstance(route, APIRoute)
+        # )
 
 
 def extract_routes(app: FastAPI) -> list[RouteInfo]:
     routes: list = []
+    for route in _iter_api_routes(app):
+        routes.append(
+            RouteInfo(
+                path=route.path,
+                endpoint=route.endpoint,
+                name=route.name,
+                methods=route.methods,
 
-    for ctx in iter_route_contexts(app.routes):
-        route: BaseRoute = ctx.original_route
-        if isinstance(route, APIRoute):
-            routes.append(
-                RouteInfo(
-                    path=route.path,
-                    endpoint=route.endpoint,
-                    name=route.name,
-                    methods=route.methods,
-
-                    # Optional Metadata
-                    status_code=route.status_code,
-                    response_model=route.response_model,
-                    summary=route.summary,
-                    description=route.description,
-                    tags=route.tags,
-                    deprecated=route.deprecated,
-                    strict_content_type=route.strict_content_type,
-                    include_in_schema=route.include_in_schema,
-                )
+                # Optional Metadata
+                status_code=route.status_code,
+                response_model=route.response_model,
+                summary=route.summary,
+                description=route.description,
+                tags=route.tags,
+                deprecated=route.deprecated,
+                strict_content_type=route.strict_content_type,
+                include_in_schema=route.include_in_schema,
             )
+        )
     return routes
