ae6964de5769290923cca7293bae29c0c8ccbbb8
Tim Jelenz <jelenzt@gmail.com>
2026-07-16 11:09:01 +0200

    add: implement `assert_issue_output_summary` and add `ast` flag
    - implement `assert_issue_output_summary` function used by test cases and
      add `ast` option in executor test case helper functions, so two behaviours
      can be tested with only one helper function

--- tests/core/test_executor.py
+++ tests/core/test_executor.py
@@ -1,27 +1,41 @@
 from fastapi_doctor.core.executor import Executor
 
-import pytest
 from json import loads
-from typing import Callable, Any
-from pathlib import Path
-from fastapi import FastAPI
-from fastapi_doctor.core.protocol import Issue, Rule
+from typing import Any
+from fastapi_doctor.core.protocol import Issue
 from fastapi_doctor.core.executor import Executor
 
 
-def assert_issue_output_summary(summary: dict[str, int | list[str]]) -> None:
+def assert_issue_output_summary(
+        summary: dict[str, list[str | int]],
+        with_ast: bool = False
+    ) -> None:
+
+    # Different methods on the same route are counted as one route with different
+    # methods. See class `RouteInfo`!
+    assert summary["total_routes"] == 5
+    assert summary["total_rules"] == 2
+
+    # `AST` is not supported by one of the sample rules
+    assert summary["issues_collected"] == 5 if not with_ast else 10
+    assert len(summary["skipped_rules"]) == (1 if not with_ast else 0)
+
+
+def assert_issue_output_routes(
+        routes: list[dict[str, Any]],
+        with_ast: bool = False
+    ) -> None:
     ...
 
 
-def assert_issue_output_routes(routes: list[dict[str, Any]]) -> None:
+def assert_issue_output_rule_issues(
+        rule_issues: dict[str, list[Issue]],
+        with_ast: bool = False
+    ) -> None:
     ...
 
 
-def assert_issue_output_rule_issues(rule_issues: dict[str, list[Issue]]) -> None:
-    ...
-
-
-def test_executor_issue_output(
+def test_executor_issue_output_with_ast(
     sample_app,
     sample_app_content,
     rule_subclass_factory,
@@ -41,9 +55,9 @@ def test_executor_issue_output(
     )()
     issues = loads(json_issues_output)
 
-    assert_issue_output_summary(issues["summary"])
-    assert_issue_output_routes(issues["routes"])
-    assert_issue_output_rule_issues(issues["rule_issues"])
+    assert_issue_output_summary(issues["summary"], with_ast=True)
+    assert_issue_output_routes(issues["routes"], with_ast=True)
+    assert_issue_output_rule_issues(issues["rule_issues"], with_ast=True)
 
 
 def test_executor_json_output_env() -> None:
